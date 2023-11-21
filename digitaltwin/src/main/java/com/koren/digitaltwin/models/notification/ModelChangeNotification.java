248a2961839261c8a9ec4deeaa527db93ea7e474
TheKoren <korenzol.007@gmail.com>
2023-11-21 16:46:09 +0100

    Added comments, finalize stabilityanalyzer

--- digitaltwin/src/main/java/com/koren/digitaltwin/models/notification/ModelChangeNotification.java
+++ digitaltwin/src/main/java/com/koren/digitaltwin/models/notification/ModelChangeNotification.java
@@ -8,6 +8,11 @@ import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+/**
+ * A specific type of notification representing a model change event in the digital twin system.
+ * Extends the AbstractNotification class and includes additional functionality or properties
+ * specific to model change notifications.
+ */
 @EqualsAndHashCode(callSuper = true)
 @Document(collection="events")
 @TypeAlias("modelChange")
@@ -16,6 +21,13 @@ import org.springframework.data.mongodb.core.mapping.Field;
 public class ModelChangeNotification extends AbstractNotification {
     @Field
     WifiMessage parent;
+    /**
+     * Constructs a ModelChangeNotification with the specified type, message, and parent WifiMessage.
+     *
+     * @param type    The type of the notification.
+     * @param message The message associated with the notification.
+     * @param parent  The parent WifiMessage associated with the model change.
+     */
     public ModelChangeNotification(NotificationType type, String message, WifiMessage parent) {
         super(type, message);
         this.parent = parent;
--- digitaltwin/src/main/java/com/koren/digitaltwin/models/notification/Notification.java
+++ digitaltwin/src/main/java/com/koren/digitaltwin/models/notification/Notification.java
@@ -4,22 +4,70 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
+/**
+ * An interface representing a notification in the digital twin system.
+ * Implementations of this interface should provide methods to access and set
+ * properties such as ID, unique key, type, and message of the notification.
+ */
 @Document(collection="events")
 public interface Notification {
+    /**
+     * Get the ID of the notification.
+     *
+     * @return The ID of the notification.
+     */
     @Id
     ObjectId getId();
+
+    /**
+     * Set the ID of the notification.
+     *
+     * @param id The ID to set for the notification.
+     */
     void setId(ObjectId id);
 
+    /**
+     * Get the unique key associated with the notification.
+     *
+     * @return The unique key of the notification.
+     */
     @Field
     String getUniqueKey();
+
+    /**
+     * Set the unique key for the notification.
+     *
+     * @param uniqueKey The unique key to set for the notification.
+     */
     void setUniqueKey(String uniqueKey);
 
+    /**
+     * Get the type of the notification.
+     *
+     * @return The type of the notification.
+     */
     @Field
     NotificationType getType();
+
+    /**
+     * Set the type of the notification.
+     *
+     * @param type The type to set for the notification.
+     */
     void setType(NotificationType type);
 
+    /**
+     * Get the message associated with the notification.
+     *
+     * @return The message of the notification.
+     */
     @Field
     String getMessage();
+
+    /**
+     * Set the message for the notification.
+     *
+     * @param message The message to set for the notification.
+     */
     void setMessage(String message);
 }
