a4da1679a6e7bcfefc6391d9912e964deb472955
TheKoren <korenzol.007@gmail.com>
2023-11-30 17:12:50 +0100

    Javadoc for Message interface

--- digitaltwin/src/main/java/com/koren/digitaltwin/models/message/Message.java
+++ digitaltwin/src/main/java/com/koren/digitaltwin/models/message/Message.java
@@ -4,10 +4,34 @@ import org.bson.types.ObjectId;
 
 import java.time.Instant;
 
+/**
+ * The {@code Message} interface represents a data structure for storing information related to a message from an IoT device.
+ * It defines methods to retrieve the unique identifier, MAC address, and timestamp of the message.
+ * Implementing classes should provide concrete implementations for these methods.
+ *
+ * @author TheKoren
+ * @version 1.0
+ * @since 2023.11.21
+ */
 public interface Message {
+    /**
+     * Retrieves the unique identifier of the message.
+     *
+     * @return The unique identifier of the message.
+     */
     ObjectId getId();
 
+    /**
+     * Retrieves the MAC address associated with the message.
+     *
+     * @return The MAC address associated with the message.
+     */
     String getMac();
 
+    /**
+     * Retrieves the timestamp indicating when the message was created.
+     *
+     * @return The timestamp of the message creation.
+     */
     Instant getTimestamp();
 }
